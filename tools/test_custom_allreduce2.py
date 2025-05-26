330ccf306563370d8789650359408128fd383dd9
cjmcv <398342899@qq.com>
2025-05-26 10:59:12 +0800

    .

--- tools/test_custom_allreduce2.py
+++ tools/test_custom_allreduce2.py
@@ -5,6 +5,7 @@ import socket
 import unittest
 from typing import Any, List, Optional
 
+import matplotlib.pyplot as plt
 import time
 import torch
 import torch.distributed as dist
@@ -27,17 +28,19 @@ def _run_correctness_worker(world_size, rank, distributed_init_port, test_sizes)
     )
     group = dist.group.WORLD
 
-    new_group = torch.distributed.new_group([0,1,2,3,4,5,6,7], backend="gloo")
+    new_group = torch.distributed.new_group(list(range(world_size)), backend="gloo")
     cop = CustomAllreduce(new_group, device)
 
     try:
         start_event = torch.cuda.Event(enable_timing=True)
         end_event = torch.cuda.Event(enable_timing=True)
-        custom_kernel_time = 0
-        nccl_kernel_time = 0
 
+        custom_perf = []
+        nccl_perf = []
         test_loop = 20
         for sz in test_sizes:
+            custom_kernel_time = 0
+            nccl_kernel_time = 0
             for dtype in [torch.bfloat16]: # torch.float32, torch.float16, 
                 for loop in range(test_loop):
                     inp1 = torch.randint(1, 16, (sz,), dtype=dtype, device=device)
@@ -61,8 +64,25 @@ def _run_correctness_worker(world_size, rank, distributed_init_port, test_sizes)
 
                     torch.testing.assert_close(out1, inp1_ref)
 
+            custom_perf.append(custom_kernel_time)
+            nccl_perf.append(nccl_kernel_time)
             print(f"custom_kernel_time: {custom_kernel_time:.6f} ms, {sz}")
             print(f"nccl_kernel_time: {nccl_kernel_time:.6f} ms, {sz}")
+
+            # plot
+            x_ticks = range(len(test_sizes))
+            plt.plot(x_ticks, custom_perf, label='custom', marker='o', markersize=3)
+            plt.plot(x_ticks, nccl_perf, label='nccl', marker='s', markersize=3)
+            plt.xticks(x_ticks, test_sizes)
+
+            plt.title(f'allreduce-WS-{world_size}-rank-{rank}.png')
+            plt.xlabel('size')
+            plt.ylabel('ms')
+
+            plt.legend()
+            plt.grid(True)
+
+            plt.savefig('allreduce-WS-{0}-rank-{1}.png'.format(world_size, rank))
 
     finally:
         dist.barrier(group=group)
@@ -116,7 +136,7 @@ class TestCustomAllReduce(unittest.TestCase):
         2097152,
         58720256,
     ]
-    world_sizes = [2, 4, 8] # [2, 4, 8]
+    world_sizes = [2, 4, 8]
 
     def test_correctness(self):
         for world_size in self.world_sizes:
