a869268ecdc4424e6eb5b506f38eaf100a4f2e5d
cjmcv <398342899@qq.com>
2025-09-19 16:06:45 +0800

    因新版cutlass已封装SM89_16x8x32_F16E4M3E4M3F16_TN，注释掉自定义的指令封装

--- include/xop/ops_impl/gemm_normal/gemm_blockscale_fp8_sm89/ada_blockwise_gemm_traits.cuh
+++ include/xop/ops_impl/gemm_normal/gemm_blockscale_fp8_sm89/ada_blockwise_gemm_traits.cuh
@@ -21,54 +21,54 @@ namespace cute
 
 // MMA 16x8x32 TN
 // ref: SM80_16x8x16_F16F16F16F16_TN vs SM80_16x8x16_F32F16F16F32_TN
-struct SM89_16x8x32_F16E4M3E4M3F16_TN
-{
-  using DRegisters = uint32_t[2];
-  using ARegisters = uint32_t[4];
-  using BRegisters = uint32_t[2];
-  using CRegisters = uint32_t[2];
+// struct SM89_16x8x32_F16E4M3E4M3F16_TN
+// {
+//   using DRegisters = uint32_t[2];
+//   using ARegisters = uint32_t[4];
+//   using BRegisters = uint32_t[2];
+//   using CRegisters = uint32_t[2];
 
-  CUTE_HOST_DEVICE static void 
-  fma(uint32_t    & d0, uint32_t    & d1,
-    uint32_t const& a0, uint32_t const& a1, uint32_t const& a2, uint32_t const& a3,
-    uint32_t const& b0, uint32_t const& b1,
-    uint32_t const& c0, uint32_t const& c1)
-  {
-#if defined(CUTE_ARCH_MMA_F16_SM89_ENABLED)
-    asm volatile(
-      "mma.sync.aligned.m16n8k32.row.col.f16.e4m3.e4m3.f16 "
-      "{%0,  %1},"
-      "{%2,  %3,  %4,  %5},"
-      "{%6,  %7},"
-      "{%8, %9};\n"
-      : "=r"(d0), "=r"(d1)
-      : "r"(a0), "r"(a1), "r"(a2), "r"(a3), 
-        "r"(b0), "r"(b1), 
-        "r"(c0), "r"(c1));
-#else
-    CUTE_INVALID_CONTROL_PATH(
-      "Attempting to use SM89_16x8x32_F16F4M3FE4M3F16_TN without "
-      "CUTE_ARCH_MMA_F16_SM89_ENABLED");
-#endif
-  }
-};
+//   CUTE_HOST_DEVICE static void 
+//   fma(uint32_t    & d0, uint32_t    & d1,
+//     uint32_t const& a0, uint32_t const& a1, uint32_t const& a2, uint32_t const& a3,
+//     uint32_t const& b0, uint32_t const& b1,
+//     uint32_t const& c0, uint32_t const& c1)
+//   {
+// #if defined(CUTE_ARCH_MMA_F16_SM89_ENABLED)
+//     asm volatile(
+//       "mma.sync.aligned.m16n8k32.row.col.f16.e4m3.e4m3.f16 "
+//       "{%0,  %1},"
+//       "{%2,  %3,  %4,  %5},"
+//       "{%6,  %7},"
+//       "{%8, %9};\n"
+//       : "=r"(d0), "=r"(d1)
+//       : "r"(a0), "r"(a1), "r"(a2), "r"(a3), 
+//         "r"(b0), "r"(b1), 
+//         "r"(c0), "r"(c1));
+// #else
+//     CUTE_INVALID_CONTROL_PATH(
+//       "Attempting to use SM89_16x8x32_F16F4M3FE4M3F16_TN without "
+//       "CUTE_ARCH_MMA_F16_SM89_ENABLED");
+// #endif
+//   }
+// };
 
-// ref: MMA_Traits<SM80_16x8x16_F16F16F16F16_TN> vs MMA_Traits<SM80_16x8x16_F32F16F16F32_TN>
-template <>
-struct MMA_Traits<SM89_16x8x32_F16E4M3E4M3F16_TN> {
-  using ValTypeD = half_t;
-  using ValTypeA = float_e4m3_t;
-  using ValTypeB = float_e4m3_t;
-  using ValTypeC = half_t;
+// // ref: MMA_Traits<SM80_16x8x16_F16F16F16F16_TN> vs MMA_Traits<SM80_16x8x16_F32F16F16F32_TN>
+// template <>
+// struct MMA_Traits<SM89_16x8x32_F16E4M3E4M3F16_TN> {
+//   using ValTypeD = half_t;
+//   using ValTypeA = float_e4m3_t;
+//   using ValTypeB = float_e4m3_t;
+//   using ValTypeC = half_t;
 
-  using Shape_MNK = Shape<_16,_8,_32>;
-  using ThrID   = Layout<_32>;
-  using ALayout = Layout<Shape <Shape < _4,_8>,Shape < _4,_2,  _2>>,
-                         Stride<Stride<_64,_1>,Stride<_16,_8,_256>>>;
-  using BLayout = Layout<Shape <Shape < _4,_8>,Shape <_4,  _2>>,
-                         Stride<Stride<_32,_1>,Stride<_8,_128>>>;
-  using CLayout = SM80_16x8_Row;
-};
+//   using Shape_MNK = Shape<_16,_8,_32>;
+//   using ThrID   = Layout<_32>;
+//   using ALayout = Layout<Shape <Shape < _4,_8>,Shape < _4,_2,  _2>>,
+//                          Stride<Stride<_64,_1>,Stride<_16,_8,_256>>>;
+//   using BLayout = Layout<Shape <Shape < _4,_8>,Shape <_4,  _2>>,
+//                          Stride<Stride<_32,_1>,Stride<_8,_128>>>;
+//   using CLayout = SM80_16x8_Row;
+// };
 
 } // namespace cute
 
