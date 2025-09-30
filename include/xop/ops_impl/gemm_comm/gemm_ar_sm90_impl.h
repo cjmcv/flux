3f21e0f2f60cf42a3807e9b27b3ec49c7bd1aa17
cjmcv <398342899@qq.com>
2025-09-30 18:15:03 +0800

    barrier_ptrs_[0]无法获得指向显存的指针！！待确认修复

--- include/xop/ops_impl/gemm_comm/gemm_ar_sm90_impl.h
+++ include/xop/ops_impl/gemm_comm/gemm_ar_sm90_impl.h
@@ -250,13 +250,14 @@ private:
     // cudaMemcpy(rank_data_, ar_args_.rank_data->ptrs, 8 * sizeof(ElementD*), cudaMemcpyDeviceToHost);
     // cudaMemcpy(&host_rank_signals_, &ar_args_.rank_signals, sizeof(vllm::RankSignals), cudaMemcpyDeviceToHost);
     for (int i=0; i<kMaxLocalWorldSize; i++) {
-      rank_data_[i] = host_rank_data_.ptrs[i];
+      rank_data_[i] = (ElementD *)host_rank_data_.ptrs[i];
 
-      cudaMemcpy(&host_signals_[i], &ar_args_.rank_signals.signals[i], sizeof(vllm::Signal*), cudaMemcpyDeviceToHost);
+      // ar_args_.rank_signals.signals[i] 是device指针，将整个Signal拷贝到内存，再基于内存索引其成员变量ptr。下面仍拷贝不成功is_device_pointer(barrier_ptrs_[0])仍然为0！！！
+      cudaMemcpy(host_signals_[i], ar_args_.rank_signals.signals[i], sizeof(vllm::Signal), cudaMemcpyDeviceToHost); // 
       barrier_ptrs_[i] = (int *)host_signals_[i]->_flag;
     }
 
-    printf("is_device_pointer: %d, %d, %d\n", is_device_pointer(barrier_ptrs_[0]), is_device_pointer(rank_data_[0]), is_device_pointer(barrier_ptrs_[0]));
+    printf("is_device_pointer: %d, %d, %d\n", is_device_pointer(barrier_ptrs_[0]), is_device_pointer(rank_data_[0]), is_device_pointer(ar_args_.rank_signals.signals[0]));
 
     arguments.rs_dma = typename GemmKernel::ReduceScatterDmaArguments{
       .output_scatter_ptrs = (ElementD **)rank_data_,
